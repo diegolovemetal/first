551ba9e294078774d9b803662b0d154bf2df87d6
diegolovemetal <2015211713@stu.cqupt.edu.cn>
2019-05-12 19:44:04 +0800

    创建文件读取文件删除文件

--- template/condition.go
+++ template/condition.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"html/template"
+	"strings"
+)
+
+func EmailDealWith(args ...interface{}) string {
+	ok := false
+	var s string
+	if len(args) == 1 {
+		s, ok = args[0].(string)
+	}
+	if !ok {
+		s = fmt.Sprint(args...)
+	}
+
+	//find @ symbol
+	substrs := strings.Split(s, "@")
+	if len(substrs) != 2 {
+		return s
+	}
+	// replace the @ by " at "
+	return (substrs[0] + "at" + substrs[1])
+}
+func main() {
+	tEmpty := template.New("template test")
+	tEmpty = template.Must(tEmpty.Parse("空 pipeline if demo:{{if ``}}不会输出.{{end}}\n"))
+	tEmpty.Execute(os.Stdout, nil)
+
+	tWithValue := template.New("template test")
+	tWithValue = template.Must(tWithValue.Parse("不为空 pipeline if demo: {{if `anything`}}我有内容，我会输出." +
+		"{{end}}\n "))
+	tWithValue.Execute(os.Stdout, nil)
+
+	tIfElse := template.New("template test")
+	tIfElse = template.Must(tIfElse.Parse("if-else demo: {{if `anything`}} if部分 {{else}} else部分{{end}}\n"))
+	tIfElse.Execute(os.Stdout, nil)
+
+
+	
+}
